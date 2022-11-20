671fdf03368d4a6493ebb658295de4b3332c627f
taeseong14 <kwontaeseong14@gmail.com>
2022-11-20 18:10:37 -0500

    .

--- krm-kakaolink.js
+++ krm-kakaolink.js
@@ -11,7 +11,7 @@ const Kakao = new KakaoLinkClient();
  * @returns {function} krm middleware
  */
 module.exports = function (email, pw, apikey, url) {
-    KakaoApiService.createService().login({
+    return KakaoApiService.createService().login({
         email: email,
         password: pw,
         keepLogin: true,
@@ -20,17 +20,16 @@ module.exports = function (email, pw, apikey, url) {
             apiKey: apikey,
             url: url
         });
+        return function (msg, reply, next) {
+            reply.kakaolink = function (template_id, template_args, room) {
+                Kakao.sendLink(room || msg.room, {
+                    template_id: template_id,
+                    template_args: template_args
+                });
+            }
+            next();
+        }
     }).catch(e => {
         throw e;
     });
-
-    return function (msg, reply, next) {
-        reply.kakaolink = function (template_id, template_args, room) {
-            Kakao.sendLink(room || msg.room, {
-                template_id: template_id,
-                template_args: template_args
-            });
-        }
-        next();
-    }
 }
